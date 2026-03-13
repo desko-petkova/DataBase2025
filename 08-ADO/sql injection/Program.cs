0122be21b426db38d3117a9d5646d11d23940100
desko-petkova <desko.petkova@gmail.com>
2026-03-13 13:07:18 +0200

    SQL injection

---
+++ 08-ADO/sql injection/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace LoginDb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Password: ");
+            string password = Console.ReadLine();//' OR '1'='1
+
+            string connectionString = @"Server=(localdb)\MSSQLLocalDB;Database=LoginDemo;Trusted_Connection=True;";
+
+            using SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+
+            string sql = "SELECT * FROM Users WHERE Username = '" + username +
+                         "' AND Password = '" + password + "'";
+
+
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                Console.WriteLine("Login successful");
+            }
+            else
+            {
+                Console.WriteLine("Login failed");
+            }
+        }
+    }
+}
